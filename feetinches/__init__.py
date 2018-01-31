1d8bcfdc7efbbaa261333d43cc1ba71cf179dee5
Cameron Dean <camhopkinsdean@gmail.com>
2018-01-31 15:30:24 +0000

    yapf

--- feetinches/__init__.py
+++ feetinches/__init__.py
@@ -1,8 +1,10 @@
 import re
 
+
 def fi_to_inches(s):
-    s = re.sub('(feet|foot|ft)', "'", re.sub('(inch|inches|in)', '"',
-        s.replace(' ', '').lower()))
+    s = re.sub('(feet|foot|ft)', "'",
+               re.sub('(inch|inches|in)', '"',
+                      s.replace(' ', '').lower()))
     if "'" in s:
         feet, i = s.split("'")
     else:
@@ -12,16 +14,17 @@ def fi_to_inches(s):
         inches = i.split('"')[0]
     else:
         inches = 0
-        
+
     return int(feet) * 12 + int(inches)
-    
+
+
 def inches_to_fi(i):
     feet = i // 12
     inches = i % 12
-    
+
     f_str = str(feet) + "'"
     i_str = str(inches) + '"'
-    
+
     if feet == 0:
         return i_str
     elif inches == 0:
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='feetinches',
-      version='0.1',
-      description='Conversion for feet-inches',
-      url='http://github.com/storborg/funniest',
-      author='Cameron Dean',
-      author_email='camhopkinsdean@gmail.com',
-      license='MIT',
-      packages=['feetinches'],
-      zip_safe=False)
+setup(
+    name='feetinches',
+    version='0.1',
+    description='Conversion for feet-inches',
+    url='http://github.com/storborg/funniest',
+    author='Cameron Dean',
+    author_email='camhopkinsdean@gmail.com',
+    license='MIT',
+    packages=['feetinches'],
+    zip_safe=False)
