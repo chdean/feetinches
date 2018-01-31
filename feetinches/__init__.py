6a6e22209d92da169d6374060d4a61d255a80599
Cameron Dean <camhopkinsdean@gmail.com>
2018-01-31 00:56:17 +0000

    initial commit

---
+++ feetinches/__init__.py
@@ -0,0 +1,30 @@
+import re
+
+def fi_to_inches(s):
+    s = re.sub('(feet|foot|ft)', "'", re.sub('(inch|inches|in)', '"',
+        s.replace(' ', '').lower()))
+    if "'" in s:
+        feet, i = s.split("'")
+    else:
+        feet = 0
+        i = s
+    if '"' in i:
+        inches = i.split('"')[0]
+    else:
+        inches = 0
+        
+    return int(feet) * 12 + int(inches)
+    
+def inches_to_fi(i):
+    feet = i // 12
+    inches = i % 12
+    
+    f_str = str(feet) + "'"
+    i_str = str(inches) + '"'
+    
+    if feet == 0:
+        return i_str
+    elif inches == 0:
+        return f_str
+    else:
+        return f_str + i_str
