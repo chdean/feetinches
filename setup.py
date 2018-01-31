b465ac62932676501aaabd6fc8eeab35181e4d7d
Cameron Dean <camhopkinsdean@gmail.com>
2018-01-31 00:58:45 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='feetinches',
+      version='0.1',
+      description='Conversion for feet-inches',
+      url='http://github.com/storborg/funniest',
+      author='Cameron Dean',
+      author_email='camhopkinsdean@gmail.com',
+      license='MIT',
+      packages=['feetinches'],
+      zip_safe=False)
